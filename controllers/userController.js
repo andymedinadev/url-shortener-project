f7b42a6bea7ecadf18f43a94ccf4a4d5fbe23b87
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-07-19 14:43:47 -0300

    feat: integrate bcrypt for password hashing in user registration and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const UserModel = require('../model/user.model')
 
 exports.userRegistration = async (req, res) => {
@@ -6,25 +7,28 @@ exports.userRegistration = async (req, res) => {
 
     const user = await UserModel.findOne({ email })
 
-    if (!user) {
-      const createUser = new UserModel({ name, email, password })
-
-      await createUser.save()
-
-      res.json({
-        success: true,
-        message: 'User created successfully',
-        data: null,
-        errors: null,
-      })
-    } else {
+    if (user) {
       res.json({
         success: false,
         message: 'This user already exists',
         data: null,
         errors: null,
       })
+      return
     }
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const createUser = new UserModel({ name, email, password: hashedPassword })
+    await createUser.save()
+
+    res.json({
+      success: true,
+      message: 'User created successfully',
+      data: null,
+      errors: null,
+    })
   } catch (error) {
     console.error(error)
   }
@@ -34,23 +38,36 @@ exports.userLogin = async (req, res) => {
   try {
     const { email, password } = req.body
 
-    const user = await UserModel.findOne({ email, password })
+    const user = await UserModel.findOne({ email })
 
-    if (user) {
-      res.json({
-        success: true,
-        message: 'Logged in!',
-        data: { user },
-        errors: null,
-      })
-    } else {
+    if (!user) {
       res.json({
         success: false,
-        message: 'User invalid, please try again',
+        message: 'User not found, please try again',
         data: null,
         errors: null,
       })
+      return
     }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      res.json({
+        success: false,
+        message: 'Password is incorrect, please try again',
+        data: null,
+        errors: null,
+      })
+      return
+    }
+
+    res.json({
+      success: true,
+      message: 'Logged in!',
+      data: { userId: user._id, email: user.email },
+      errors: null,
+    })
   } catch (error) {
     console.error(error)
   }
