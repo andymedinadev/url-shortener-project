3f227d8b8b55276c1501ca3e9ebd27c483dc8640
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-07-19 15:17:56 -0300

    feat: improve server address configuration using environment variables

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+require('dotenv').config()
+
 const app = require('./app')
 
+let displayAddress
+
+const PROTOCOL = process.env.PROTOCOL || 'http'
+const DOMAIN_NAME = process.env.DOMAIN_NAME || 'localhost'
+const PORT = process.env.PORT || 3000
+
+const USE_STANDARD_HTTP_PORTS = process.env.USE_STANDARD_HTTP_PORTS === 'true'
+
+if (USE_STANDARD_HTTP_PORTS) {
+  displayAddress = `${PROTOCOL}://${DOMAIN_NAME}/`
+} else {
+  displayAddress = `${PROTOCOL}://${DOMAIN_NAME}:${PORT}/`
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('The server is running!')
 })
 
-app.listen(3000, () => {
-  console.log('Server running in port 3000')
-  console.log('http://localhost:3000/')
+app.listen(PORT, () => {
+  console.log(`Server running in:  ${displayAddress}`)
 })
